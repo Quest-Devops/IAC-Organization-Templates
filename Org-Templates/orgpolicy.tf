ffeeb91ccc3c9274682fccea56c145599344cae1
karandhawariyaji <karandhawariyaji@gmail.com>
2024-11-20 19:14:16 +0530

    Update orgpolicy.tf

--- Org-Templates/orgpolicy.tf
+++ Org-Templates/orgpolicy.tf
@@ -2,7 +2,7 @@ module "org_policy" {
   for_each = {
     for k, v in try(var.org_policy,{}): k => v if v.delete != true
   }
-  source           = "git::https://[PASSWORD]@github.com/Quest-Devops/IAC-Organization-Modules.git//terraform-google-cloud-org-policy?ref=v1.0.0"
+  source           = "git::https://[PASSWORD]@github.com/Quest-Devops/IAC-Organization-Modules.git//terraform-google-cloud-org-policy?ref=v1.0.1"
   org_id           = each.value.org_id
   constraint       = each.key
   policy_version   = each.value.version
