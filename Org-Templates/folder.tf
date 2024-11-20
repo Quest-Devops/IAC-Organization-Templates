0250ee1d86d61d6cca42d89d2a8ad6096559d455
karandhawariyaji <karandhawariyaji@gmail.com>
2024-11-20 19:14:03 +0530

    Update folder.tf

--- Org-Templates/folder.tf
+++ Org-Templates/folder.tf
@@ -4,7 +4,7 @@ module "Level_2_BU_Folders" {
   for_each     = {  
     for k, v in try(var.Level_2_BU_Folders,{}): k => v if v.delete != true            
   }     
-  source        =  "git::https://[PASSWORD]@github.com/Quest-Devops/IAC-Organization-Modules.git//terraform-google-cloud-folder?ref=v1.0.0"       
+  source        =  "git::https://[PASSWORD]@github.com/Quest-Devops/IAC-Organization-Modules.git//terraform-google-cloud-folder?ref=v1.0.1"       
   folder_name         = each.key
   tags                = each.value.tags
   deletion_protection = each.value.deletion_protection
@@ -20,7 +20,7 @@ module "Level_3_boot_BU_Folders" {
     for k, v in try(var.Level_3_boot_BU_Folders,{}): k => v if v.delete != true
   }
 
-  source        =  "git::https://[PASSWORD]@github.com/Quest-Devops/IAC-Organization-Modules.git//terraform-google-cloud-folder?ref=v1.0.0"
+  source        =  "git::https://[PASSWORD]@github.com/Quest-Devops/IAC-Organization-Modules.git//terraform-google-cloud-folder?ref=v1.0.1"
   folder_name  = each.key
   parent       = module.Level_2_BU_Folders["gcp-boot-poc"].folder_details.id
   tags         = each.value.tags
@@ -32,7 +32,7 @@ module "Level_3_us_BU_Folders" {
   for_each     = {
     for k, v in try(var.Level_3_us_BU_Folders,{}): k => v if v.delete != true
   }
-  source        =  "git::https://[PASSWORD]@github.com/Quest-Devops/IAC-Organization-Modules.git//terraform-google-cloud-folder?ref=v1.0.0"  
+  source        =  "git::https://[PASSWORD]@github.com/Quest-Devops/IAC-Organization-Modules.git//terraform-google-cloud-folder?ref=v1.0.1"  
   folder_name  = each.key
   parent       = module.Level_2_BU_Folders["gcp-us-poc"].folder_details.id
   tags         = each.value.tags
@@ -49,7 +49,7 @@ module "Level_4_us_dev_BU_Folders" {
   for_each     = {
     for k, v in try(var.Level_4_us_dev_BU_Folders,{}): k => v if v.delete != true
   }
-  source        =  "git::https://[PASSWORD]@github.com/Quest-Devops/IAC-Organization-Modules.git//terraform-google-cloud-folder?ref=v1.0.0" 
+  source        =  "git::https://[PASSWORD]@github.com/Quest-Devops/IAC-Organization-Modules.git//terraform-google-cloud-folder?ref=v1.0.1" 
   folder_name  = each.key
   parent       = module.Level_3_us_BU_Folders["gcp-dev-us-poc"].folder_details.id
   tags         = each.value.tags
